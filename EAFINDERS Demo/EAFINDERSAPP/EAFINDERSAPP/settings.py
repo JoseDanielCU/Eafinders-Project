0ca3966d64bc26eb077ea75385eda40b0a68cfcf
Jose Daniel Castañeda Urdinola <jdcastaneu@eafit.edu.co>
2024-11-06 19:16:43 -0500

    Update

--- EAFINDERS Demo/EAFINDERSAPP/EAFINDERSAPP/settings.py
+++ EAFINDERS Demo/EAFINDERSAPP/EAFINDERSAPP/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'EAFINDERSAPP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'App/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
